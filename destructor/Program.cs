232a91b1add65065408cab5aff9adab72db4e31a
Qui Ly <lquiitfuture@gmail.com>
2019-12-12 21:56:35 +0700

    add all files

---
+++ destructor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace destructor
+{
+    // Destructor in C#
+    class DestructorDemo
+    {
+        public DestructorDemo()
+        {
+            Console.WriteLine("constructor object created");
+        }
+        ~DestructorDemo()
+        {
+            Console.WriteLine("object is destroyed");
+        }
+    }
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            DestructorDemo obj1 = new DestructorDemo();
+            DestructorDemo obj2 = new DestructorDemo();
+            obj1 = null;
+            obj2 = null;
+            GC.Collect();
+            Console.ReadKey();
+        }
+    }
+
+}
